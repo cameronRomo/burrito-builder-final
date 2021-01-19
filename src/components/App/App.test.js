39a56c051d8c491dc56c7f7b2172e67de183a289
Cameron Romo <cromo1225@gmail.com>
2021-01-19 13:55:20 -0700

    Change logic with before each

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -6,8 +6,7 @@ import '@testing-library/jest-dom';
 import {getOrders} from '../../apiCalls';
 jest.mock('../../apiCalls');
 
-const mockedOrders = { 
-  orders: [{
+const mockedOrders = [{
     id: 1,
     name: 'Cam',
     ingredients: ['carnitas']
@@ -16,18 +15,18 @@ const mockedOrders = {
     id: 2,
     name: 'Snuffleupagus',
     ingredients: ['beans']
-  }]
-}
+  }
+]
 
 
 describe('App', () => {
   beforeEach(() => {
     getOrders.mockResolvedValue(mockedOrders);
-    render(
-      <App />
-      )
   })
   it('should render successfully', () => {
+     render(
+      <App />
+      )
     
     const header = screen.getByText('Burrito Builder');
 
@@ -35,17 +34,14 @@ describe('App', () => {
   })
 
   it('should add a new order', async () => {
-    getOrders.mockResolvedValue(mockedOrders);
     render(
       <App />
       )
-
     const orderNameInput = await waitFor(() => screen.getByPlaceholderText(/Name/i));
     const ingredient = screen.getByRole('button', { name: /beans/i });
     const submitButton = screen.getByRole('button', { name: /submit order/i });
     
-    getOrders.mockResolvedValueOnce({orders: [...mockedOrders, {name: 'Human', ingredients: ['beans'], id: 3}]})
-
+    getOrders.mockResolvedValueOnce([...mockedOrders, {name: 'Human', ingredients: ['beans'], id: 3}])
     fireEvent.type(orderNameInput, 'Human');
     fireEvent.click(ingredient);
     fireEvent.click(submitButton);
